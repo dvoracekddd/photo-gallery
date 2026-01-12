356cd224a6fa9e250635f63141e8fd389f5a7a02
David Dvořáček <d_dvoracek@utb.cz>
2026-01-12 19:51:23 +0100

    Adding Platform and Capacitor for support mobile platforms, editing the photoservice for posibility to use as mobile system or as web for saving and for loading photos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -3,14 +3,24 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import type { Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
+import { Platform } from '@ionic/angular';
+import { Capacitor } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhotoService {
   public photos: UserPhoto[] = [];
-  //Key for photo storage
+  
+  // Key for photo storage
   private PHOTO_STORAGE: string = 'photos';
+
+  // Property to track the app's running platform
+  private platform: Platform;
+
+  constructor(platform: Platform) {
+    this.platform = platform;
+  }
 
   public async addNewToGallery() {
     // Take a photo
@@ -24,18 +34,31 @@ export class PhotoService {
 
     this.photos.unshift(savedImageFile);
     
-    //method to cache all photo data for future retrieval
+    // Method to cache all photo data for future retrieval
     Preferences.set({
       key: this.PHOTO_STORAGE,
       value: JSON.stringify(this.photos),
     });
   }
 
+
   private async savePicture(photo: Photo) {
-    // Fetch the photo, read as a blob, then convert to base64 format
-    const response = await fetch(photo.webPath!);
-    const blob = await response.blob();
-    const base64Data = (await this.convertBlobToBase64(blob)) as string;
+    let base64Data: string | Blob;
+
+    // Platform check
+    // "hybrid" will detect mobile - iOS or Android
+    if (this.platform.is('hybrid')) {
+      // Read the file into base64 format
+      const file = await Filesystem.readFile({
+        path: photo.path!
+      });
+      base64Data = file.data;
+    } else {
+      // Fetch the photo, read as a blob, then convert to base64 format
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      base64Data = await this.convertBlobToBase64(blob) as string;
+    }
 
     // Write the file to the data directory
     const fileName = Date.now() + '.jpeg';
@@ -45,12 +68,21 @@ export class PhotoService {
       directory: Directory.Data,
     });
 
-    // Use webPath to display the new image instead of base64 since it's
-    // already loaded into memory
-    return {
-      filepath: fileName,
-      webviewPath: photo.webPath,
-    };
+    // Platform check
+    if (this.platform.is('hybrid')) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      };
+    }
   }
 
   private convertBlobToBase64(blob: Blob) {
@@ -71,16 +103,18 @@ export class PhotoService {
     const photoList = result.value;
     this.photos = (photoList ? JSON.parse(photoList) : []) as UserPhoto[];
 
-    //Display the photo by reading into base64 format
-    for (let photo of this.photos) {
-      // Read each saved photo's data from the Filesystem
-      const readFile = await Filesystem.readFile({
-        path: photo.filepath,
-        directory: Directory.Data,
-      });
+    // Platform check
+    // If running on the web display the photo by reading into base64 format
+    if (!this.platform.is('hybrid')) {
+      for (let photo of this.photos) {
+        const readFile = await Filesystem.readFile({
+            path: photo.filepath,
+            directory: Directory.Data
+        });
 
-      // Web platform only: Load the photo as base64 data
-      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+        // Web platform only: Load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
     }
   }
 }
